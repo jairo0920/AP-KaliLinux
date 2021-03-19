236b8dd9e865578b1ad5ce7b1a40e611563c0079
Jairo Alberto Cruz Sibaja <jairo@cruz.cr>
2021-03-19 15:06:13 +0000

    first commit

---
+++ RogueSploit/BeEFStarter.sh
@@ -0,0 +1,16 @@
+Copyright (c) @2016-2017 H0nus
+#!/bin/bash
+DATE=$(date)
+echo $DATE
+LOG_FILE=$PWD/beef.log
+exec > >(tee -a ${LOG_FILE} )
+exec 2> >(tee -a ${LOG_FILE} >&2)
+echo $DATE > $PWD/beef.log
+echo "[*]Starting the BeEF Framework and Panel..."
+echo "[*]Go to http://10.0.0.1:3000/ui/panel for control BeEF"
+echo "[*]More infos will appear right now..."
+echo "[*]Login is:"
+echo "[!]User: RogueSploit"
+echo "[!]Password: pwnonair"
+cd /usr/share/beef-xss/ && sudo ruby beef
+
